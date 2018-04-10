fda1cc56a4d9bec0ad81ecdb73fd1c4de7921efe
sevenSpade <35969021+sevenSpade@users.noreply.github.com>
2018-04-10 21:09:40 +0100

    commit

--- main.php
+++ main.php
@@ -1,10 +1,10 @@
 <?php
+/* njarreb fel commit */
 	include('verificationAuthentification.php');
 	echo '<header>\n';
 	include('header.php');
 	echo '\n</header>';
 ?>
-
 <body>
 	<img src="main/ane_4.jpg">;
 	<div>

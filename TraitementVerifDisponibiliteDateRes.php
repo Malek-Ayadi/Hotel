69ba78945b29f2cb8b312c44a3ec4027eef0357b
Skander <meziane.skander@outlook.fr>
2018-04-13 12:30:28 +0200

    Mise à jour des enregistrements

---
+++ TraitementVerifDisponibiliteDateRes.php
@@ -0,0 +1,36 @@
+<?php
+	try
+	{
+		$bdd= new PDO('mysql:host=localhost;dbName=projet', 'root','');
+	}
+	catch(Exception $e)
+	{
+	    die('Erreur : '.$e->getMessage());
+
+	}
+
+	/*Requête : Rechercher les id des chambres qui ne se trouvent pas dans la table réservation et donc ne sont pas réservées
+	  ou bien les id des chambres réservées mais dans une date différente que celle demandée par le client 	
+	  -->ON VA RENVOYER LA PREMIERE CHAMBRE LIBRE !!	
+	*/
+	
+	$req=$bdd->prepare('SELECT C.id_chamb FROM projet.chambre C, projet.reservation R WHERE ( C.id_chamb not in (SELECT A.id_chamb FROM projet.reservation A WHERE A.id_chamb=C.id_chamb) AND C.type=? ) OR ((C.id_chamb=R.id_chamb) AND (R.dateDep<? OR R.dateArr>? ) AND C.type=?)');
+	$req->execute(array($_POST['type'],$_POST['dateArr'],$_POST['dateDep'],$_POST['type']));
+	$final=$req->fetch();
+	
+	if ($final['id_chamb']==NULL)
+	{
+		header("Location:VerifierDisponibiliteDateRes.php?dispo=n");
+		exit();
+	}
+	else
+	{	
+		$dateArr=$_POST['dateArr'];
+		$dateDep=$_POST['dateDep'];
+		$chambre=$final['id_chamb'];
+		header("Location:enregistrement.php?chambre=$chambre&dateArr=$dateArr&dateDep=$dateDep");
+		exit();
+	}
+	
+
+?>
--- enregistrementClt.php
+++ enregistrementClt.php
@@ -1,5 +1,5 @@
 <?php
-	include '../verificationAuthentification.php';
+	//include '../verificationAuthentification.php';
 
 ?>
 <!DOCTYPE html>
@@ -8,15 +8,15 @@
 		<title>enregistrementClt</title>
 	</head>
 	<body>
-		<form action="traitementEnregistrementClt.php?cin=<?php echo ($_GET['cin']) ?>" method="POST">
-			<p><label for="nom">Nom: </label><input type="text" name="nom" id="nom"></p>
-			<p><label for="prenom">Prénom: </label><input type="text" name="prenom" id="prenom"></p>
-			<p><label for="cin">CIN: </label><input type="text" name="cin" id="cin" value="<?php echo ($_GET['cin']) ?>"></p>
-			<p><label for="ddn">Date de naissance: </label><input type="date" name="ddn" id="ddn"></p>
-			<p><label for="mail">E-mail: </label><input type="text" name="mail" id="mail"></p>
-			<p><label for="adresse">Adresse: </label><input type="text" name="adresse" id="adresse"></p>
-			<p><label for="tel">Tél: </label><input type="text" name="tel" id="tel"></p>
-			<p><label for="Reserver"></label><input type="submit" name="Reserver" id="Reserver" value="Réserver" ></p>
+		<form action="traitementEnregistrementClt.php?cin=<?php echo ($_GET['cin']) ?>&chambre=<?php echo ($_GET['chambre']) ?>&dateDep=<?php echo ($_GET['dateDep']) ?>&dateArr=<?php echo ($_GET['dateArr']) ?>" method="POST">
+			<p><label for="nom">Nom: </label><input required type="text" name="nom" id="nom"></p>
+			<p><label for="prenom">Prénom: </label><input required type="text" name="prenom" id="prenom"></p>
+			<p><label for="cin">CIN: </label><input required readonly type="text" name="cin" id="cin" value="<?php echo ($_GET['cin']) ?>"></p>
+			<p><label for="ddn">Date de naissance: </label><input required type="date" name="ddn" id="ddn"></p>
+			<p><label for="mail">E-mail: </label><input required type="email" name="mail" id="mail"></p>
+			<p><label for="adresse">Adresse: </label><input required type="text" name="adresse" id="adresse"></p>
+			<p><label for="tel">Tél: </label><input required type="text" name="tel" id="tel"></p>
+			<p><label for="Reserver"></label><input required type="submit" name="Reserver" id="Reserver" value="Réserver" ></p>
 		</form>
 
 

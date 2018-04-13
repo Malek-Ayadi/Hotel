9d7dbf3a29f18f688a5ccaa6c23408d8d0a74405
Skander <meziane.skander@outlook.fr>
2018-04-13 13:29:40 +0200

    ajout required

--- consommation.php
+++ consommation.php
@@ -13,17 +13,17 @@
 	} 
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Consommation</title>
-    <meta charset="utf-8">
-</head>
-<body>
-<form method="post" action="traitementConsommation.php">
-    <label for="id_produit">ID produit</label><input type="number" name="id_produit" id="id_produit">
-    <label for="id_clt">ID client</label><input type="number" name="id_clt" id="id_clt">
-    <input type="submit" name="valider" value="valider">
-</form>
+	<html>
+	<head>
+	    <title>Consommation</title>
+	    <meta charset="utf-8">
+	</head>
+	<body>
+	<form method="post" action="traitementConsommation.php">
+	    <label for="id_produit">ID produit</label><input type="number" required name="id_produit" id="id_produit">
+	    <label for="id_clt">ID client</label><input type="number" required name="id_clt" id="id_clt">
+	    <input type="submit" name="valider" value="valider">
+	</form>
 
-</body>
-</html>
+	</body>
+	</html>

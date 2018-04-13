62a987009a190ff9857219ca7b9caa4c6ef6b889
Skander <meziane.skander@outlook.fr>
2018-04-13 16:06:26 +0200

    Correction ModificationPersonnel

--- traitementModifierPersonnel.php
+++ traitementModifierPersonnel.php
@@ -13,21 +13,27 @@
 
 	}
 
-	$bdd->query("USE projet");
 	if((isset($_POST['admin']))and($_POST['admin']=='on'))
+	{
 		$admin=1;
-	else $admin=0;
-	$req=$bdd->prepare('UPDATE produit SET login = :login, password = :password,nom=:nom,prenom=:prenom,mail=:mail,admin=:admin WHERE employee.login =:login');
-	$req->execute(array
-		(
-			'login'=>$_POST['login'],
-			'password'=>$_POST['password'],
-			'nom'=>$_POST['nom'],
-			'prenom'=>$_POST['prenom'],
-			'mail'=>$_POST['mail'],
-			'admin'=>$admin,
-			'login'=>$_GET['login']
+	}
+	else
+	{
+		$admin=0;
+	} 
+	
+		
+		$req = $bdd->prepare ('UPDATE projet.employee SET login=?, password=?, nom=?, prenom=?, mail=?, admin=? WHERE login = ?');
+		$req->execute(array(
+			$_POST['login'],
+			$_POST['password'],
+			$_POST['nom'],
+			$_POST['prenom'],
+			$_POST['mail'],
+			$admin,
+			$_GET['login']
+
 		));
-	header("Location: espaceAdmin.php");
+		header("Location: espaceAdmin.php");
 	
 ?>

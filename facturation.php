2a66c5bb1a1fb7a204a5f693e9e818a0cefe784d
yahya <derbeliy97@gmail.com>
2018-04-13 22:10:35 +0100

    facturation css+bg

--- facturation.php
+++ facturation.php
@@ -36,10 +36,11 @@
     <title>Facturation</title>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="style.css">
+     <link href="./css/background.css" rel="stylesheet">
 </head>
 <body>
-	<div class="container col-6">
-	<div class="border border-primary" style="margin:200px; padding:50px">
+	<div class="container col-12">
+    <div  style="margin-right:700px; margin-top: 200px;margin-bottom: 200px; padding-left:50px; padding-right: 50px " >
 <form method="post" action="facturation.php?ok=1">
     <label for="cin">Cin</label> <input type="number" name="cin" class="form-control form-control-lg">
     <input type="submit" name="entrer" value="entrer"  class="btn btn-primary">

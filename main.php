35907d01e03de1d8951118b308ccc3c07e36c541
yahya <derbeliy97@gmail.com>
2018-04-13 13:39:52 +0100

    maj main.php

--- main.php
+++ main.php
@@ -3,47 +3,11 @@ include("verificationAuthentification.php");
 include("choixNavigation.php");
 ?>
 <body>
-  <style src="vendor/bootstrap/css/bootstrap.min.css"></style>
-  <style>
-.container1 {
-  position: relative;
-  width: 200px;
-  height:200px;
-}
+  <link src="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"></style>
+  
+  <link href="css/face.css" rel="stylesheet">
 
-.image1 {
-  display: block;
-  width: 100%;
-  height: auto;
-}
 
-.overlay1 {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 100%;
-  width: 100%;
-  opacity: 0;
-  transition: .5s ease;
-  background-color: #008CBA;
-}
-
-.container1:hover .overlay1 {
-  opacity: 1;
-}
-
-.text1 {
-  color: white;
-  font-size: 20px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-}
-</style>
 
 <table class="table table-bordered" style="margin-bottom: 38px;">
  

194552e446521e3f8c535cf78e61cc767061ce39
yahya <derbeliy97@gmail.com>
2018-04-13 13:09:17 +0100

    CSS verifier dispo

--- verifierDisponibiliteDateRes.php
+++ verifierDisponibiliteDateRes.php
@@ -1,24 +1,33 @@
+<?php
+	include 'verificationAuthentification.php';
+	include 'choixNavigation.php'; ?>
 <!DOCTYPE html>
 	<html>
 	<head>
+
 		<title>Verifier Disponibilité</title>
 		<meta charset="utf-8">
+		<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	</head>
 	<body>
+		<div class="container col-12">
+			<div class="border border-primary" style="margin:201px; padding-left:100px; padding-right: 100px; " >
 		<form action="TraitementVerifDisponibiliteDateRes.php" method="POST">
 			<p>
 				<label for="type">Type de la chambre: </label>
-				<select size="1" name="type" id="type" required> 
+				<select size="1" name="type" id="type" required class="form-control form-control-lg"> 
 					<option value="simple">Simple</option> 
 					<option value="double">Double</option> 
 					<option value="triple">Triple</option>
 				</select>
 			</p>
 
-			<p><label for="dateArr">Date d'arrivée: </label><input type="date" name="dateArr" id="dateArr" required></p>
-			<p><label for="dateDep">Date de départ: </label><input type="date" name="dateDep" id="dateDep" required></p>
-			<p><input type="submit" name="valider" id="valider"></p>
+			<p><label for="dateArr">Date d'arrivée: </label><input type="date" name="dateArr" id="dateArr" required class="form-control form-control-lg"></p>
+			<p><label for="dateDep">Date de départ: </label><input type="date" name="dateDep" id="dateDep" required class="form-control form-control-lg"></p>
+			<p><input type="submit" name="valider" id="valider" class="btn btn-primary"></p>
 		</form>
+	</div>
+</div>
 
 			<script type="text/javascript">
 				var dateArr=document.getElementById('dateArr');
@@ -34,5 +43,6 @@
 						
 				});
 			</script>
+	<?php include 'piedPage.php';  ?>
 	</body>
 	</html>

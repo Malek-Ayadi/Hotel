16d3f2bb0ade0b333ba51e59af4956e8fec2e31e
ahmedhajyahmed <hajyahmedboh@yahoo.com>
2018-04-13 13:55:34 +0200

    rien

---
+++ espaceAdminJs.js
@@ -0,0 +1,6 @@
+var nod=document.getElementById("enployee");
+console.log(nod);
+nod.addEventListener('click',function () {
+    //document.location.href="ajouterPersonnel.php";
+    document.write('<?php include("ajouterPersonnel.php") ?>');
+});

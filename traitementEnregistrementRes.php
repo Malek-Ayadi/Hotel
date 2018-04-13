3cd3a6070aa7c163ae5b17725ec79f22a369140f
Skander <meziane.skander@outlook.fr>
2018-04-13 13:56:32 +0200

    modification enregistrementRes

--- traitementEnregistrementRes.php
+++ traitementEnregistrementRes.php
@@ -13,12 +13,13 @@
 	}
 
 	
-	$req=$bdd->prepare('INSERT INTO projet.reservation (id_clt,dateArr,dateDep) VALUES (?,?,?)');
+	$req=$bdd->prepare('INSERT INTO projet.reservation (id_clt,dateArr,dateDep,id_chamb) VALUES (?,?,?,?)');
 	$req->execute(array
 		(	
 			$_POST['id_clt'],
 			$_POST['dateArr'],
-			$_POST['dateDep']
+			$_POST['dateDep'],
+			$_POST['id_chamb']
 		));
 
 	header("Location: reservation.php");

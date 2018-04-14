4b02bafd3f20d5b1f500c12266abd8ddb7ff37de
Skander <meziane.skander@outlook.fr>
2018-04-14 15:34:42 +0200

    MAJ ReadOnly EnregistrementRes.php

--- enregistrementRes.php
+++ enregistrementRes.php
@@ -38,10 +38,10 @@
 		<div class="container col-12">
 	<div  style="margin-right:700px; margin-top: 100px;margin-bottom: 100px; padding-left:50px; padding-right: 50px " >
 		<form action="traitementEnregistrementRes.php" method="POST">
-			<p><label for="id_clt">Id client: </label><input required type="number" value="<?php echo ("$id")?>" name="id_clt" id="id_clt" class="form-control form-control-lg"></p>
-			<p><label for="id_clt">Numéro Chambre: </label><input required type="number" value="<?php echo ("$chambre")?>" name="id_chamb" id="id_chamb" class="form-control form-control-lg"></p>
-			<p><label for="dateArr">Date d'arrivee: </label><input type="date" value="<?php echo ("$dateArr")?>" name="dateArr" id="dateArr" required class="form-control form-control-lg"></p>
-			<p><label for="dateDep">Date de départ: </label><input type="date" value="<?php echo ("$dateDep")?>" name="dateDep" id="dateDep" required class="form-control form-control-lg"></p>
+			<p><label for="id_clt">Id client: </label><input required readonly type="number" value="<?php echo ("$id")?>" name="id_clt" id="id_clt" class="form-control form-control-lg"></p>
+			<p><label for="id_clt">Numéro Chambre: </label><input readonly required type="number" value="<?php echo ("$chambre")?>" name="id_chamb" id="id_chamb" class="form-control form-control-lg"></p>
+			<p><label for="dateArr">Date d'arrivee: </label><input readonly type="date" value="<?php echo ("$dateArr")?>" name="dateArr" id="dateArr" required class="form-control form-control-lg"></p>
+			<p><label for="dateDep">Date de départ: </label><input type="date" value="<?php echo ("$dateDep")?>" name="dateDep" id="dateDep" required readonly class="form-control form-control-lg"></p>
 			<input type="submit" name="reserver" value="Réserver"  class="btn btn-primary">
 		</form>
 	</div>

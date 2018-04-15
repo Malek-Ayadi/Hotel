ce7dc1f9e3fc2af6c90f6912d43201ce22b86e73
Skander <meziane.skander@outlook.fr>
2018-04-15 20:44:34 +0200

    Correction enregistrement
    Cas où table réservation vide !!

--- TraitementVerifDisponibiliteDateRes.php
+++ TraitementVerifDisponibiliteDateRes.php
@@ -13,24 +13,39 @@
 	  ou bien les id des chambres réservées mais dans une date différente que celle demandée par le client 	
 	  -->ON VA RENVOYER LA PREMIERE CHAMBRE LIBRE !!	
 	*/
-	
-	$req=$bdd->prepare('SELECT C.id_chamb FROM projet.chambre C, projet.reservation R WHERE ( C.id_chamb not in (SELECT A.id_chamb FROM projet.reservation A WHERE A.id_chamb=C.id_chamb) AND C.type=? ) OR ((C.id_chamb=R.id_chamb) AND (R.dateDep<? OR R.dateArr>? ) AND C.type=?)');
-	$req->execute(array($_POST['type'],$_POST['dateArr'],$_POST['dateDep'],$_POST['type']));
-	$final=$req->fetch();
-	
-	if ($final['id_chamb']==NULL)
+	  $req=$bdd->query ('SELECT * FROM projet.reservation ');
+	if (!$req->fetch())
 	{
-		header("Location:VerifierDisponibiliteDateRes.php?dispo=non");
-		exit();
-	}
-	else
-	{	
+		$req=$bdd->prepare('SELECT C.id_chamb FROM projet.chambre C WHERE C.type=?');
+		$req->execute(array($_POST['type']));
+		$final=$req->fetch();
 		$dateArr=$_POST['dateArr'];
 		$dateDep=$_POST['dateDep'];
 		$chambre=$final['id_chamb'];
 		header("Location:enregistrement.php?chambre=$chambre&dateArr=$dateArr&dateDep=$dateDep");
 		exit();
 	}
+	else
+	{
+		$req=$bdd->prepare('SELECT C.id_chamb FROM projet.chambre C, projet.reservation R WHERE ( C.id_chamb not in (SELECT A.id_chamb FROM projet.reservation A WHERE A.id_chamb=C.id_chamb) AND C.type=? ) OR ((C.id_chamb=R.id_chamb) AND (R.dateDep<? OR R.dateArr>? ) AND C.type=?)');
+		$req->execute(array($_POST['type'],$_POST['dateArr'],$_POST['dateDep'],$_POST['type']));
+		$final=$req->fetch();
+		
+		if ($final['id_chamb']==NULL)
+		{
+			header("Location:VerifierDisponibiliteDateRes.php?dispo=non");
+			exit();
+		}
+		else
+		{	
+			$dateArr=$_POST['dateArr'];
+			$dateDep=$_POST['dateDep'];
+			$chambre=$final['id_chamb'];
+			header("Location:enregistrement.php?chambre=$chambre&dateArr=$dateArr&dateDep=$dateDep");
+			exit();
+		}
+	}
+	
 	
 
 ?>

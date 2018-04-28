e6a62e2907b5d2a9abd1e0e030d3bc51c49bd205
karouiamine <karouiamine@users.noreply.github.com>
2018-04-28 14:15:36 +0200

    Réctification traitementConsommation

--- traitementConsommation.php
+++ traitementConsommation.php
@@ -11,30 +11,7 @@ catch(Exception $e)
     die('Erreur : '.$e->getMessage());
 
 }
-echo ($_POST['id_produit']);
-$produit=0;
-$clt=0;
-$req1=$bdd->query('SELECT id_clt FROM projet.client');
-while ($aff=$req1->fetch())
-{
 
-    if ($aff['id_clt']==$_POST['id_clt'])
-	{
-		$clt=1;
-		break;
-	}
-}
-$req2=$bdd->query('SELECT id_produit FROM projet.produit ');
-while ($aff=$req2->fetch())
-{
-	if ($aff['id_produit']==$_POST['id_produit'])
-     {
-     	$produit =1;
-     	break;
-	 }
-}
-if(($produit==1)&&($clt==1))
-	{
 		$req=$bdd->prepare('INSERT INTO projet.consommation (id_clt,id_produit) VALUES (?,?)');
             $req->execute(array
             (
@@ -43,18 +20,6 @@ if(($produit==1)&&($clt==1))
             ));
             header("Location: consommation.php?err");
             exit();
-	}
-else if(($produit==0)&&($clt==0))
-	{
-		header("Location: consommation.php?err1");
-	}
-else if($clt==0)
-	{
-		header("Location: consommation.php?err2");
-	}
-else if($produit==0)
-	{
-		header("Location: consommation.php?err3");
-	}
+
 
 	?>
